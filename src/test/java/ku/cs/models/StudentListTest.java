552504753b38ce1193fcb927274d15b7fc5db124
Perthanakrit <aupper094@gmail.com>
2024-08-22 17:29:33 +0700

    add test case in StudentList

--- src/test/java/ku/cs/models/StudentListTest.java
+++ src/test/java/ku/cs/models/StudentListTest.java
@@ -38,17 +38,33 @@ class StudentListTest {
         studentList.addNewStudent("66xxxxxx1", "s1", 50);
         studentList.addNewStudent("66xxxxxx2", "s2", 55);
 
-        String userId = studentList.getStudents().get(0).getId();
+        String userId = studentList.getStudents().get(1).getId();
         studentList.giveScoreToId(userId, 11.11);
 
-        assertEquals(66.11, studentList.getStudents("66xxxxxx1"));
+        assertEquals(66.11, studentList.findStudentById(userId).getScore());
     }
 
     @Test
     void viewGradeOfId() {
+        studentList.addNewStudent("66xxxxxx1", "s1", 50);
+        studentList.addNewStudent("66xxxxxx2", "s2", 55);
+
+        String userId = studentList.getStudents().get(0).getId();
+        String grade = studentList.viewGradeOfId(userId);
+
+        assertEquals("D", grade);
     }
 
     @Test
     void getStudents() {
+        studentList.addNewStudent("66xxxxxx1", "s1", 50);
+        studentList.addNewStudent("66xxxxxx2", "s2", 55);
+        studentList.addNewStudent("66xxxxxx3", "s3", 60);
+
+        String students = "[{id: '66xxxxxx1', name: 's1', score: 50.0}, " +
+                            "{id: '66xxxxxx2', name: 's2', score: 55.0}, " +
+                            "{id: '66xxxxxx3', name: 's3', score: 60.0}]";
+
+        assertEquals(students.toString(), studentList.getStudents().toString());
     }
 }
